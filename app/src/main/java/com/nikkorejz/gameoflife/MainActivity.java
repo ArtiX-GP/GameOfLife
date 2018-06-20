9ec02283f3a41318b5fda43eb7b7a631620504ea
Nikita G <nikita_gromik@mail.ru>
2018-06-20 17:58:10 +0800

    Update MainActivity.java
    Добавил комментарии

--- app/src/main/java/com/nikkorejz/gameoflife/MainActivity.java
+++ app/src/main/java/com/nikkorejz/gameoflife/MainActivity.java
@@ -19,11 +19,16 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // Ширина сетки.
     private final int WIDTH_GRID = 25;
+    
+    // Высота сетки.
     private final int HEIGHT_GRID = 10;
 
+    // Двумерный массив (сетка, таблица) для рассчета хода.
     private boolean[][] mArray = new boolean[HEIGHT_GRID][];
 
+    // Таймер
     private CountDownTimer mTimer;
 
     private boolean isGameStarted = false;
@@ -32,46 +37,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Контейнер с кнопками (ячейками).
         FrameLayout mButtonsContainer = new FrameLayout(this);
+        
+        // Ширина и высота кнопки (Кнопки квадратные)
         int WIDTH_BUTTON = 75;
 
+        // Параметры для ячеек.
         LinearLayout.LayoutParams mParams = new LinearLayout.LayoutParams(WIDTH_BUTTON, WIDTH_BUTTON);
         for (int i = 0; i < HEIGHT_GRID; i++) {
+            // На каждую строку создаем массив из *WIDTH_GRID* элементов.
             mArray[i] = new boolean[WIDTH_GRID];
             for (int j = 0; j < WIDTH_GRID; j++) {
+                // Создаем ячейку.
                 ImageButton mButton = new ImageButton(this);
+                // Подключаем слушатель событий.
                 mButton.setOnClickListener(MainActivity.this);
+                
+                // Настройка позиционирования ячейки (добавляем отступы).
                 mParams.leftMargin = WIDTH_BUTTON * j;
                 mParams.topMargin = WIDTH_BUTTON * i;
+                
+                // Применяем изменения.
                 mButton.setLayoutParams(mParams);
+                
+                // Устанавливаем уникальный идентификатор.
                 mButton.setId(100 * i + j);
+                
+                // Устанавливаем задний фон для ячейки.
                 mButton.setBackground(ContextCompat.getDrawable(this, R.drawable.background));
+                
+                // Добавляем ячейку в ранее созданный контейнер.
                 mButtonsContainer.addView(mButton);
             }
         }
 
+        // Параметры для контейнера.
         FrameLayout.LayoutParams mParams2 = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        
+        // Центрируем все кнопки по центру.
         mParams2.gravity = Gravity.CENTER;
+        
+        // Применяем изменения.
         mButtonsContainer.setLayoutParams(mParams2);
 
+        // Показываем то, что получилось. 
         setContentView(mButtonsContainer);
 
+        // Запускаем таймер.
         onCreateTimer();
     }
 
     private void onCreateTimer() {
+        // Если таймер живой, то останавливаем его.
         if (mTimer != null) {
             mTimer.cancel();
         }
+        
+        // Создаем новый таймер.
         mTimer = new CountDownTimer(10000, 100) {
             @Override
             public void onTick(long millisUntilFinished) {
+                // Если игра запущена, то выполняем один шаг каждые 100 миллисекунд.
                 if (isGameStarted)
                     doStep();
             }
 
             @Override
             public void onFinish() {
+                // По окончании таймера запускаем его заново (создаем эффект бесконечной анимации).
                 mTimer.start();
             }
         };
@@ -81,10 +115,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @SuppressLint("ResourceType")
     @Override
     public void onClick(View v) {
+        // При нажатии на любую ячейку изменяем ее статус.
         v.setSelected(!v.isSelected());
+        // Также меняем состояние в массиве.
         mArray[v.getId() / 100][v.getId() % 100] = v.isSelected();
     }
 
+    // Функция дублирования двумерного массива.
     boolean[][] duplicate() {
         boolean[][] mDuplicate = new boolean[HEIGHT_GRID][];
         for (int i = 0; i < HEIGHT_GRID; i++) {
@@ -95,10 +132,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     void doStep() {
+        // Создаем дубликат, для корректной работы программы.
+        // Мы не можем проверять одну матрицу и сразу же делать в ней изменения, это повлечет за собой ошибки.
         boolean[][] mDuplicate = duplicate();
         for (int i = 0; i < HEIGHT_GRID; i++) {
             for (int j = 0; j < WIDTH_GRID; j++) {
+                // Кол-во соседей вокруг данной клеточки.
                 int mCount = 0;
+                
+                // Считаем соседей.
+                
                 if (i > 0 && mDuplicate[i - 1][j])
                     mCount++;
                 if (i > 0 && j > 0 && mDuplicate[i - 1][j - 1])
@@ -115,12 +158,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     mCount++;
                 if (i < HEIGHT_GRID - 1 && j < WIDTH_GRID - 1 && mDuplicate[i + 1][j + 1])
                     mCount++;
+                
+                // Посчитали соседей.
 
+                // Если данная клетка "жива", то если кол-во соседей равно 2 или 3, то оставляем эту клетку в покое.
                 if (mDuplicate[i][j]) {
                     mArray[i][j] = mCount == 2 || mCount == 3;
-                } else {
+                } else { // Иначе, клетка оживает в том случае, если кол-во соседей равно 3.
                     mArray[i][j] = mCount == 3;
                 }
+                // Находим нашу ячейку (кнопку) и обновляем ее статус (Жива она или нет).
                 findViewById(100 * i + j).setSelected(mArray[i][j]);
             }
         }
@@ -128,12 +175,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Мы это не проходили, это создание контекстного меню.
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Слушатель нажатий на элементы меню.
         switch (item.getItemId()) {
             case R.id.ActionPlay:
                 isGameStarted = true;
@@ -152,6 +201,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStop() {
         super.onStop();
+        // Когда мы закрываем приложение, то мы выключаем таймер.
         if (mTimer != null)
             mTimer.cancel();
     }
